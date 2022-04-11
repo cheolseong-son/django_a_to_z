4b34533e0af52a1ffbf8df4ada44241d1575b1b9
cheolseong-son <github-email>
2022-04-11 17:17:34 +0900

    author 필드 추가하기

--- blog/models.py
+++ blog/models.py
@@ -17,9 +17,11 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
 # Create your models here.
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'

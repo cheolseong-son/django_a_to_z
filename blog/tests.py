e42fcf926c0cd50f9476c177d6adf75e5c5d50b1
cheolseong-son <github-email>
2022-04-11 17:41:39 +0900

    포스트 목록, 상세 페이지에서 작성자 정보 출력하기

--- blog/tests.py
+++ blog/tests.py
@@ -1,10 +1,15 @@
 from django.test import TestCase, Client
+from django.contrib.auth.models import User
 from bs4 import BeautifulSoup
 from .models import Post
 
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
+        self.user_trump = User.objects.create_user(username='trump',
+                                                   password='somepassword')
+        self.user_obama = User.objects.create_user(username='obama',
+                                                   password='somepassword')
 
     def navbar_test(self):
         navbar = soup.nav
@@ -52,10 +57,12 @@ class TestView(TestCase):
         post_001 = Post.objects.create(
             title = '첫 번째 포스트입니다.',
             content = 'Hello World. we are the world',
+            author = self.user_trump
         )
         post_002 = Post.objects.create(
             title='두 번째 포스트입니다.',
             content='1등이 전부는 아니잖아요',
+            author = self.user_obama
         )
         self.assertEqual(Post.objects.count(), 2)
         # 3.2 포스트 목록 페이지를 새로고침했을 때
@@ -71,20 +78,25 @@ class TestView(TestCase):
 
         # '아직 게시물이 없습니다.'라는 문구는 더 이상 보이지 않는다.
         self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
+
+        # author가 대문자로 출력되게 함
+        self.assertIn(self.user_trump.username.upper(), main_area)
+        self.assertIn(self.user_obama.username.upper(), main_area)
 # Create your tests here.
 
     def test_post_detail(self):
         # 1.1 Post가 하나 있다.
-        post_001 = Post.objects.create(
+        post_000 = Post.objects.create(
             title = '첫 번째 포스트입닏.',
             content = 'Hello world. we are the world',
+            author = self.user_trump,
         )
         # 1.2 그 포스트의 url은 '/blog/1/'이다.
-        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+        self.assertEqual(post_000.get_absolute_url(), '/blog/1/')
 
         # 2. 첫 번재 포스트의 상세 페이지 테스트
         # 2.1 첫 번째 post url로 접근하면 정상적으로 작동한다.()
-        response = self.client.get(post_001.get_absolute_url())
+        response = self.client.get(post_000.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
 
@@ -95,15 +107,16 @@ class TestView(TestCase):
         self.navbar_test(soup)
 
         # 2.3 첫 번째 포스트의 제목이 웹 브라우저 탭 타이틀에 있다.
-        self.assertIn(post_001.title, soup.title.text)
+        self.assertIn(post_000.title, soup.title.text)
 
         # 2.4 첫 번째 포스트의 제목이 포스트 역역(post_area)에 있다.
         main_area = soup.find('div', id='main-area')
         post_area = main_area('div', id='post-area')
-        self.assertIn(post_001.title, post_area.text)
+        self.assertIn(post_000.title, post_area.text)
 
         # 2.5 첫 번째 포스트의 작성자가 포스트 영역에 있다.
+        self.assertIn(self.user_trump.username.upper(), post_area.text)
 
         # 2.6 첫 번째 포스트 내용이 포스트 영역에 있다.
-        self.assertIn(post_001.content, post_area.text)
+        self.assertIn(post_000.content, post_area.text)
 

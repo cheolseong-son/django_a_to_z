61f1bf7b4d25faf4c624740d51e17f969eb0b18d
cheolseong-son <github-email>
2022-04-11 17:27:00 +0900

    삭제된 작성자는 None으로 표시하기

--- blog/models.py
+++ blog/models.py
@@ -16,9 +16,11 @@ class Post(models.Model):
     file_upload = models.FileField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+    # 사용자가 작한 모든 것이 삭제됨
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-
+    # 사용자가 작성한 글은 남고 작성자명을 빈칸으로
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'

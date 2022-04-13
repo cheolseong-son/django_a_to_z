1a2286628e1b60d7fe2e04a4533c669fe434e1f4
cheolseong-son <github-email>
2022-04-13 18:40:17 +0900

    댓글 삭제 기능 추가하기

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     # path('search/<str:q>/', views.PostSearch.as_view()),
-    # path('delete_comment/<int:pk>/', views.delete_comment),
+    path('delete_comment/<int:pk>/', views.delete_comment),
     path('update_comment/<int:pk>/', views.CommentUpdate.as_view()),
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('create_post/', views.PostCreate.as_view()),
